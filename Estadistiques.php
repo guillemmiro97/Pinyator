a6688fe8c5b22d92fe0ccdc49c4d316feaa667d9
elputorei <elputorei@gmail.com>
2018-08-02 15:19:27 +0200

    #5 Nova pantalla per buscar pinyes anteriors

--- Estadistiques.php
+++ Estadistiques.php
@@ -8,35 +8,33 @@
 <?php $menu=8; include "$_SERVER[DOCUMENT_ROOT]/pinyator/Menu.php";?>
 <?php include "$_SERVER[DOCUMENT_ROOT]/pinyator/Connexio.php";?>
 <div class="sidenav" id="posicio" style="width:200px;" >
-<br>
+	<br>
 
-<button class="boto" style="margin-bottom:10px" onClick="ModificaiFrame('Estadistiques/Assistencia.php')">Gràfica assistència</button>
+	<button class="boto"  onClick="ModificaiFrame('Estadistiques/Assistencia.php')">Gràfica assistència</button>
+	<hr>
 
+	<select class="form_edit" id="castell">
+	<?php
 
-<select class="form_edit" id="castell">
-<?php
+		$sql="SELECT DISTINCT NOM
+		FROM CASTELL
+		ORDER BY NOM ";
 
-	$sql="SELECT DISTINCT NOM
-	FROM CASTELL
-	ORDER BY NOM ";
+		$result = mysqli_query($conn, $sql);
 
-	$result = mysqli_query($conn, $sql);
-
-	if (mysqli_num_rows($result) > 0) 
-	{
-		while($row = mysqli_fetch_assoc($result))
-		{			
-			echo "<option value=".$row["NOM"].">".$row["NOM"]."</option>";
+		if (mysqli_num_rows($result) > 0) 
+		{
+			while($row = mysqli_fetch_assoc($result))
+			{			
+				echo "<option value='".$row["NOM"]."'>".$row["NOM"]."</option>";
+			}
 		}
-	}
-?>
-</select>
-<button class="boto" style="margin-bottom:10px" title="Cal seleccionar el castell" onClick="ModificaiFrame2('Estadistiques/Castell_Posicio_estadistiques_taula.php')">Posició per casteller</button>
-
-
-<button class="boto" style="margin-bottom:10px" onClick="ModificaiFrame('Estadistiques/Assistencia_Casteller_estadistiques_taula.php')">Assistència</button>
-
-
+	?>
+	</select>
+	<button class="boto" style="margin-bottom:10px" title="Cal seleccionar el castell" onClick="ModificaiFrame2('Estadistiques/Castells_Fets.php')">Assaig/Actuació</button>
+	<button class="boto" title="Cal seleccionar el castell" onClick="ModificaiFrame2('Estadistiques/Castell_Posicio_estadistiques_taula.php')">Posició per casteller</button>
+	<hr>
+	<button class="boto" onClick="ModificaiFrame('Estadistiques/Assistencia_Casteller_estadistiques_taula.php')">Assistència</button>
 </div>
 <div style="margin-left:200px">
 <iframe id="mainFrame" src="" width="100%" height="90%" style="border:0" ></iframe>
---
+++ Estadistiques/Castells_Fets.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+</head>
+<link rel="stylesheet" href="../Style_Custom.css">
+<body>
+<?php include "$_SERVER[DOCUMENT_ROOT]/pinyator/Connexio.php";?>
+
+<?php
+$Castell_nom=strval($_GET["id"]);
+if (!empty($Castell_nom))
+{	
+	class Event
+	{ 
+		public $Castells=array();
+		public $Nom = ""; 
+		public $Id = 0;
+
+		public function __construct()
+		{
+			$this->Castells = array();
+			$this->Nom = "";
+			$this->Id = 0;
+		}		
+	}
+	
+	$Events=array();
+
+	$sql="SELECT CASTELL_ID, C.NOM, E.NOM AS EVENT,	C.EVENT_ID AS EVENT_ID
+	FROM CASTELL C
+	INNER JOIN EVENT AS E ON E.EVENT_ID=C.EVENT_ID
+	WHERE C.NOM='".$Castell_nom."'
+	ORDER BY E.DATA";
+	
+	
+	$result = mysqli_query($conn, $sql);
+
+	if (mysqli_num_rows($result) > 0) 
+	{
+		$anterior = 0;
+		while($row = mysqli_fetch_assoc($result)) 
+		{
+			$event = $row["EVENT_ID"];
+			if ($event != $anterior)
+			{
+				$objEvent = new Event();
+				$objEvent->Castells = array();
+				$objEvent->Nom = $row["EVENT"];
+				$objEvent->Id = $row["EVENT_ID"];				
+				
+				array_push($Events, $objEvent);				
+			}
+			$objEvent->Castells[$row["CASTELL_ID"]] = $row["NOM"];
+			
+			$anterior = $event;
+		}	
+	}
+	
+	//Printem la taula
+	echo "<table class='llistes'>";
+	echo "<tr class='llistes'>
+			<th class='llistes'>Event</th>
+			<th class='llistes'>Castell</th>
+		</tr>";
+
+	foreach($Events as $evnt) 
+	{
+		echo "<tr class='llistes'>
+				<td class='llistes' rowspan=".count($evnt->Castells)."><a href='..\Castell.php?id=".$evnt->Id."' target='_blank'>".$evnt->Nom."</a></td>";
+			
+		foreach($evnt->Castells as $key => $value)
+		{
+			echo "<td class='llistes'><a href='..\Castell_Fitxa.php?id=".$key."' target='_blank'>".$value."</a></td>";
+			if (count($evnt->Castells) > 1)
+			{
+				echo "</tr>";
+			}
+		}
+		
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+
+?>	
+</body>
+</html>
