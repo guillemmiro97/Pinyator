42d4690ac54c928f2c52bba2b354a986f97bc2bb
elputorei <elputorei@gmail.com>
2019-11-24 22:19:36 +0100

    La gràfica ara esta ordenada per dia

--- Estadistiques/Assistencia.php
+++ Estadistiques/Assistencia.php
@@ -13,6 +13,7 @@ $temporada=strval($_GET["t"]);
 
 $i = 1;
 $assaigs=array();
+$assaigsData=array();
 $assaigsNom=array();
 $temporades=array();
 $str="";
@@ -38,39 +39,34 @@ $str="";
 	}
 
 
-	$sql="SELECT DATA, NOM, TEMPORADA, Y, rownum, rownumT 
-	FROM (
-		SELECT *,
-		@running:=if(@previous=E.TEMPORADA,@running,0) + 1 as rownum,
-        @previous:=E.TEMPORADA as prev,
-		@runningT:=if(@previousT=E.TEMPORADA,0,1) + @runningT as rownumT,
-        @previousT:=E.TEMPORADA as prevT
-		FROM(
-			SELECT E.DATA, E.NOM, E.TEMPORADA, 
+	$sql="SELECT *,
+			@running:=if(@previous=E.DATA,@running,@running+1) as rownum,
+			@previous:=E.DATA as prev
+		FROM (
+			SELECT CONCAT(DAY(DATA),'/',MONTH(DATA)) AS DATA, E.NOM, E.TEMPORADA, 
 			SUM(IF(I.ESTAT > 0, 1, 0)) AS Y
 			FROM EVENT AS E 
 			LEFT JOIN INSCRITS AS I ON I.EVENT_ID=E.EVENT_ID
 			LEFT JOIN CASTELLER AS C ON C.CASTELLER_ID=I.CASTELLER_ID
 			LEFT JOIN POSICIO AS P ON P.POSICIO_ID=C.POSICIO_PINYA_ID
 			JOIN    (SELECT @running := 0) r
-			JOIN    (SELECT @previous := 0) r2
-			JOIN    (SELECT @runningT := 0) v
-			JOIN    (SELECT @previousT := 0) v2
+			JOIN    (SELECT @previous := '') r2
 			WHERE E.TIPUS = 0
 			AND E.ESTAT >= 0
 			AND (E.TEMPORADA = '".$temporada."' OR '0' = '".$temporada."') 
 			AND (EVENT_PARE_ID IS NULL OR EVENT_PARE_ID < 1)
 			AND (P.ESNUCLI=1 OR P.ESTRONC=1 OR P.ESCORDO=1)
 			GROUP BY E.TEMPORADA, E.DATA, E.NOM
-			ORDER BY E.DATA) AS E
-	)AS T1 ORDER BY rownumT, rownum";
+			ORDER BY MONTH(E.DATA), DAY(E.DATA)) AS E
+			";
 
 	$result = mysqli_query($conn, $sql);
 	
 	if (mysqli_num_rows($result) > 0) 
 	{
-		array_push($assaigs, array_fill(1, mysqli_num_rows($result), array_fill(1, count($temporades), 0)));
-		array_push($assaigsNom, array_fill(1, mysqli_num_rows($result), array_fill(1, count($temporades), 0)));
+		array_push($assaigsData, array_fill(1, 300, ''));
+		array_push($assaigs, array_fill(1, 300, array_fill(1, count($temporades), 0)));
+		array_push($assaigsNom, array_fill(1, 300, array_fill(1, count($temporades), 0)));
 		
 		/*for($x = 1; $x < count($assaigs); $x++)
 		{
@@ -86,43 +82,55 @@ google.charts.setOnLoadCallback(drawBasic);
 
 function drawBasic() {
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'X');";
-	  for($x = 1; $x < count($temporades); $x++)
-	  {
-		echo "data.addColumn('number', '".$temporades[$x]."');";
-		echo "data.addColumn({type: 'number', role: 'annotation'});";
-		echo "data.addColumn({type: 'string', role: 'tooltip'});";
-	  }
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'X');";
+		for($x = 1; $x < count($temporades); $x++)
+		{
+			echo "data.addColumn('number', '".$temporades[$x]."');";
+			echo "data.addColumn({type: 'number', role: 'annotation'});";
+			echo "data.addColumn({type: 'string', role: 'tooltip'});";
+		}
 
-      echo "data.addRows([";
+
+		
+		echo "data.addRows([";
 		// output data of each row
 		while($row = mysqli_fetch_assoc($result)) 
 		{
-			$assaigs[$row["rownum"]][$row["rownumT"]] = $row["Y"];
-			$assaigsNom[$row["rownum"]][$row["rownumT"]] = $row["NOM"];	
+			$sy = array_search($row["TEMPORADA"], $temporades);
+			$sx = intval($row["rownum"]);
+			$assaigs[$sx][$sy] = $row["Y"];
+			$assaigsNom[$sx][$sy] = $row["NOM"];
+			$assaigsData[$sx] = $row["DATA"];
 		}
 		
 		for($x = 1; $x < count($assaigs); $x++)
 		{
-			$str = "";
+			$str = "'".$assaigsData[$x]."'";
 			for($y = 1; $y < count($temporades); $y++)
 			{
-				$str .= ",".(empty($assaigs[$x][$y])?0:$assaigs[$x][$y]);
-				$str .= ",".(empty($assaigs[$x][$y])?0:$assaigs[$x][$y]);
+				$str .= ",".(empty($assaigs[$x][$y])?"":$assaigs[$x][$y]);
+				$str .= ",".(empty($assaigs[$x][$y])?"":$assaigs[$x][$y]);
 				$str .= ",'".(empty($assaigsNom[$x][$y])?"":$assaigsNom[$x][$y])."'";
 			}
 
 			if ($x == 1)
-				echo "[".$x.$str."]";
+			{
+				echo "[".$str."]";
+			}
 			else
-				echo ",[".$x.$str."]";
+			{
+				echo ",[".$str."]";
+			}
 		}
 		echo "]);
 
       var options = {
+		interpolateNulls: true,
+		fontSize:13,
         hAxis: {
-          title: 'Assaig'
+          title: 'Assaig',
+		  showTextEvery: 0
         },
         vAxis: {
           title: 'Inscrits'
