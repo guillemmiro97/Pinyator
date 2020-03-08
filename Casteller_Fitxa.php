0638cc5161e0643fcad04ebab2e6d59bdcd20992
elputorei <elputorei@gmail.com>
2020-03-08 18:13:27 +0100

    #46 Afegit Tabs per veure dades o estadistiques

--- Casteller_Fitxa.php
+++ Casteller_Fitxa.php
@@ -4,6 +4,52 @@
 	<?php include "$_SERVER[DOCUMENT_ROOT]/pinyator/Head.php";?>
 </head>
 <?php include "$_SERVER[DOCUMENT_ROOT]/pinyator/Style.php";?>
+
+<style>
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+  position:absolute;
+  left:330px;
+  width:500px;
+  z-index:10;
+}
+
+/* Style the buttons that are used to open the tab content */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+  position:absolute;
+  left:330px;
+  width:500px;
+  padding-top:60px;
+}
+</style>
 <body>
 <?php $menu=1; include "$_SERVER[DOCUMENT_ROOT]/pinyator/Menu.php";
 
@@ -31,8 +77,6 @@
 	$portarpeu = 1;
 	$urlInscripcio = "";
 	$novell = 0;
-				
-	echo "<form method='post' action='Casteller_Desa.php'>";
 
 	if ($id > 0)
 	{
@@ -73,17 +117,18 @@
 		}
 	}
 ?>
-	<div>
-		<table class="butons">
-			<tr>
-				<th><a href="Casteller.php" class="boto" >Torna als castellers</a></th>
-			</tr>
-		</table>
-	</div> 
-	<div id="panellLateral" style="position:absolute;padding-top:10px;width:320px;height:100%">
+
+	<div id="panellLateral" style="position:absolute;width:320px;height:100%">
 		<iframe id="mainFrame" <?php echo $urlInscripcio ?> width="100%" height="90%" style="border:0" ></iframe>
 	</div>
-	<div style="position:absolute;left:320px;width:500px">
+		
+	<div class="tab">
+	  <button class="tablinks" onclick="openTab(event, 'Dades')">Dades</button>
+	  <button class="tablinks" onclick="openTab(event, 'Estadistiques')">Estadístiques</button>
+	</div>
+	
+<form method="post" action="Casteller_Desa.php">
+	<div id="Dades" class="tabcontent active">
 		<div class="form_group">
 			<label>ID - </label><?php echo "  <a href='Inscripcio.php?id=".$url."'>Link als seus esdeveniments</a>"?>
 			<input type="text" class="form_edit" name="id" value="<?php echo $id ?>" readonly>
@@ -274,8 +319,9 @@
 			<button type="Submit" name= "Desa" value="desarievents" class="boto" style="float:right">Desa sense sortir</button>
 		</div>
 	</div>
-	</form>
-	<div id="panellLateral2" style="position:absolute;right:10px;padding-top:10px;width:350px;height:100%">
+</form>
+	
+	<div id="Estadistiques" class="tabcontent">
 		<label>TEMPORADA</label>
 		<select class="form_edit" id="temporada" onchange="ModificaiFrame()">
 		<?php
@@ -308,6 +354,30 @@
 		function ModificaiFrame()
 		{
 			document.getElementById('frameEstadistiques').src = "Casteller_estadistiques_taula.php?id=<?php echo $id ?>&t=" + document.getElementById('temporada').value;
+		}
+		
+		function openTab(evt, tabName) 
+		{
+			// Declare all variables
+			var i, tabcontent, tablinks;
+
+			// Get all elements with class="tabcontent" and hide them
+			tabcontent = document.getElementsByClassName("tabcontent");
+			for (i = 0; i < tabcontent.length; i++) 
+			{
+				tabcontent[i].style.display = "none";
+			}
+
+			// Get all elements with class="tablinks" and remove the class "active"
+			tablinks = document.getElementsByClassName("tablinks");
+			for (i = 0; i < tablinks.length; i++) 
+			{
+				tablinks[i].className = tablinks[i].className.replace(" active", "");
+			}
+
+			// Show the current tab, and add an "active" class to the button that opened the tab
+			document.getElementById(tabName).style.display = "block";
+			evt.currentTarget.className += " active";
 		}
 	</script>
 </body>
